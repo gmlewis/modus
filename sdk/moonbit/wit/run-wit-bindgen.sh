852ce67689d80c5cf0498e888dd41a8d84d39a7f
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-19 15:49:55 -0400

    Move generated files into place
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- sdk/moonbit/wit/run-wit-bindgen.sh
+++ sdk/moonbit/wit/run-wit-bindgen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 rm -rf ffi interface pkg world
 wit-bindgen moonbit --derive-show --derive-eq --derive-error --gen-dir pkg .
-rm -rf pkg/world* world
+mv interface/modus/core/* interface
+rm -rf pkg/world* world moon.mod.json
+rep 'modus/core/' 'gmlewis/modus/wit/' pkg/moon.pkg.json interface/*/moon.pkg.json
