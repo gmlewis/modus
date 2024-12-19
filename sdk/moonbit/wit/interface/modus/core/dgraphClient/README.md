100db0d2577f5bee600c019999361b4e989bf75b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-19 15:35:04 -0400

    Add wit-gen generated files
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ sdk/moonbit/wit/interface/modus/core/dgraphClient/README.md
@@ -0,0 +1,1 @@
+Main dgraph client interface
--- sdk/moonbit/wit/run-wit-bindgen.sh
+++ sdk/moonbit/wit/run-wit-bindgen.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -ex
+rm -rf ffi interface pkg world
 wit-bindgen moonbit --derive-show --derive-eq --derive-error --gen-dir pkg .
+rm -rf pkg/world* world
