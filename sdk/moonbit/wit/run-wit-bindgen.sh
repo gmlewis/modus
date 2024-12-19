e90a668985ca7a0a6fa2c561b8006ef62036ec8e
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-19 16:52:53 -0400

    Update wit definitions
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- sdk/moonbit/wit/run-wit-bindgen.sh
+++ sdk/moonbit/wit/run-wit-bindgen.sh
@@ -4,3 +4,6 @@ wit-bindgen moonbit --derive-show --derive-eq --derive-error --gen-dir pkg .
 mv interface/modus/core/* interface
 rm -rf pkg/world* world moon.mod.json
 rep 'modus/core/' 'gmlewis/modus/wit/' pkg/moon.pkg.json interface/*/moon.pkg.json
+rep 'modus:core/' 'modus_' interface/*/ffi.mbt
+rep -- '-client' '_client' interface/*/ffi.mbt
+moon fmt
