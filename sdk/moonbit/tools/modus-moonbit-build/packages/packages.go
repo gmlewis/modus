83bf5248550043c975c94b5a18ff16bb5456f154
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-01-14 10:06:18 -0500

    Support testing with wasm-gc, js, and native
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- sdk/moonbit/tools/modus-moonbit-build/packages/packages.go
+++ sdk/moonbit/tools/modus-moonbit-build/packages/packages.go
@@ -206,8 +206,9 @@ type MoonPkgJSON struct {
 	IsMain     bool              `json:"is-main,omitempty"`
 	Imports    []json.RawMessage `json:"import,omitempty"`
 	TestImport []json.RawMessage `json:"test-import,omitempty"`
-	// Targets is a map of filenames followed by its compilation rules.
-	// e.g. "file_notjs.mbt": ["not", "js"], "file_js.mbt": ["js"], etc.
+	// Targets is a map of filenames (keys) followed by its compilation rules
+	// (which is an array of strings).
+	// e.g. "file_notwasm.mbt": ["not", "wasm"], "file_wasm.mbt": ["wasm"], etc.
 	Targets map[string][]string `json:"targets,omitempty"`
 	// LinkTargets is a map of link targets (e.g. "wasm", "js", etc.)
 	// followed by its exports.
@@ -216,8 +217,10 @@ type MoonPkgJSON struct {
 
 // LinkTarget represents the JSON format of a link target in moon.pkg.json.
 type LinkTarget struct {
-	Exports          []string `json:"exports,omitempty"`
-	ExportMemoryName string   `json:"export-memory-name,omitempty"`
+	// Exports is a list of exported functions in the resulting wasm module.
+	Exports []string `json:"exports,omitempty"`
+	// ExportMemoryName is the name of the exported memory (e.g. "memory").
+	ExportMemoryName string `json:"export-memory-name,omitempty"`
 }
 
 // A Package describes a loaded MoonBit package.
