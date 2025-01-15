1e19f7fcf1d3b25122c68f829c7b83161c3bad9c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-01-15 16:54:09 -0500

    Add debugging
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- runtime/languages/moonbit/typeinfo.go
+++ runtime/languages/moonbit/typeinfo.go
@@ -12,6 +12,7 @@ package moonbit
 import (
 	"context"
 	"errors"
+	"log"
 	"reflect"
 	"strings"
 
@@ -32,67 +33,88 @@ func GetTypeInfo(ctx context.Context, typeName string, typeCache map[string]lang
 type langTypeInfo struct{}
 
 func (lti *langTypeInfo) GetListSubtype(typ string) string {
-	return typ[strings.Index(typ, "]")+1:] // TODO(gmlewis)
+	result := typ[strings.Index(typ, "]")+1:] // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: GetListSubtype('%v') = '%v'", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) GetMapSubtypes(typ string) (string, string) {
+	log.Printf("GML: typeinfo.go: GetMapSubtypes('%v') = '','' - not implemented yet", typ)
 	return "", "" // TODO(gmlewis)
 }
 
 func (lti *langTypeInfo) GetNameForType(typ string) string {
+	log.Printf("GML: typeinfo.go: GetNameForType('%v') = '' - not implemented yet", typ)
 	return "" // TODO(gmlewis)
 }
 
 func (lti *langTypeInfo) IsObjectType(typ string) bool {
-	return !lti.IsPrimitiveType(typ) &&
+	result := !lti.IsPrimitiveType(typ) &&
 		!lti.IsListType(typ) &&
 		!lti.IsMapType(typ) &&
 		!lti.IsStringType(typ) &&
 		!lti.IsTimestampType(typ) &&
 		!lti.IsPointerType(typ)
+	log.Printf("GML: typeinfo.go: IsObjectType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) GetUnderlyingType(typ string) string {
-	return strings.TrimPrefix(typ, "*") // TODO(gmlewis)
+	result := strings.TrimPrefix(typ, "*") // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: GetUnderlyingType('%v') = '%v' - TODO", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsListType(typ string) bool {
-	return len(typ) > 2 && typ[0] == '[' // TODO(gmlewis)
+	result := len(typ) > 2 && typ[0] == '[' // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsListType('%v') = %v - TODO", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsSliceType(typ string) bool {
-	return len(typ) > 2 && typ[0] == '[' && typ[1] == ']' // TODO(gmlewis)
+	result := len(typ) > 2 && typ[0] == '[' && typ[1] == ']' // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsSliceType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsArrayType(typ string) bool {
-	return len(typ) > 2 && typ[0] == '[' && typ[1] != ']' // TODO(gmlewis)
+	result := len(typ) > 2 && typ[0] == '[' && typ[1] != ']' // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsArrayType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsBooleanType(typ string) bool {
-	return typ == "Bool"
+	result := typ == "Bool"
+	log.Printf("GML: typeinfo.go: IsBooleanType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsByteSequenceType(typ string) bool {
 	switch typ {
 	case "Array[Byte]", "Bytes", "ArrayView[Byte]", "BytesView":
+		log.Printf("GML: typeinfo.go: IsByteSequenceType('%v') = true", typ)
 		return true
 	}
 
 	if lti.IsArrayType(typ) {
 		switch lti.GetListSubtype(typ) {
 		case "Byte":
+			log.Printf("GML: B: typeinfo.go: IsByteSequenceType('%v') = true", typ)
 			return true
 		}
 	}
 
+	log.Printf("GML: typeinfo.go: IsByteSequenceType('%v') = false", typ)
 	return false
 }
 
 func (lti *langTypeInfo) IsFloatType(typ string) bool {
 	switch typ {
 	case "Float", "Double":
+		log.Printf("GML: typeinfo.go: IsFloatType('%v') = true", typ)
 		return true
 	default:
+		log.Printf("GML: typeinfo.go: IsFloatType('%v') = false", typ)
 		return false
 	}
 }
@@ -102,43 +124,59 @@ func (lti *langTypeInfo) IsIntegerType(typ string) bool {
 	case "Int", "Int64",
 		"Uint", "Uint64",
 		"Byte", "Char": // TODO(gmlewis)
+		log.Printf("GML: typeinfo.go: IsIntegerType('%v') = true - TODO", typ)
 		return true
 	default:
+		log.Printf("GML: typeinfo.go: IsIntegerType('%v') = false - TODO", typ)
 		return false
 	}
 }
 
 func (lti *langTypeInfo) IsMapType(typ string) bool {
-	return strings.HasPrefix(typ, "Map[")
+	result := strings.HasPrefix(typ, "Map[")
+	log.Printf("GML: typeinfo.go: IsMapType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsNullableType(typ string) bool {
-	return lti.IsPointerType(typ) || lti.IsSliceType(typ) || lti.IsMapType(typ) // TODO(gmlewis)
+	result := lti.IsPointerType(typ) || lti.IsSliceType(typ) || lti.IsMapType(typ) // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsNullableType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsPointerType(typ string) bool {
-	return strings.HasPrefix(typ, "*") // TODO(gmlewis)
+	result := strings.HasPrefix(typ, "*") // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsPointerType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsPrimitiveType(typ string) bool {
-	return lti.IsBooleanType(typ) || lti.IsIntegerType(typ) || lti.IsFloatType(typ) // TODO(gmlewis)
+	result := lti.IsBooleanType(typ) || lti.IsIntegerType(typ) || lti.IsFloatType(typ) // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsPrimitiveType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsSignedIntegerType(typ string) bool {
 	switch typ {
 	case "Int", "Int64": // TODO(gmlewis)
+		log.Printf("GML: typeinfo.go: IsSignedIntegerType('%v') = true", typ)
 		return true
 	default:
+		log.Printf("GML: typeinfo.go: IsSignedIntegerType('%v') = false", typ)
 		return false
 	}
 }
 
 func (lti *langTypeInfo) IsStringType(typ string) bool {
-	return typ == "String"
+	result := typ == "String"
+	log.Printf("GML: typeinfo.go: IsStringType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) IsTimestampType(typ string) bool {
-	return typ == "time.Time" // TODO(gmlewis)
+	result := typ == "time.Time" // TODO(gmlewis)
+	log.Printf("GML: typeinfo.go: IsTimestampType('%v') = %v", typ, result)
+	return result
 }
 
 func (lti *langTypeInfo) ArrayLength(typ string) (int, error) {
@@ -152,6 +190,7 @@ func (lti *langTypeInfo) GetAlignmentOfType(ctx context.Context, typ string) (ui
 func (lti *langTypeInfo) ObjectsUseMaxFieldAlignment() bool {
 	return true // TODO(gmlewis)
 }
+
 func (lti *langTypeInfo) GetDataSizeOfType(ctx context.Context, typ string) (uint32, error) {
 	return lti.GetSizeOfType(ctx, typ)
 }
