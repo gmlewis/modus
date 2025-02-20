37bfdc249e40d00cff83f8a80162d42fc7f778ed
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-02-20 08:33:30 -0500

    Improve myWasmMock
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- runtime/languages/moonbit/handler_memory_test.go
+++ runtime/languages/moonbit/handler_memory_test.go
@@ -11,7 +11,9 @@ package moonbit
 
 import (
 	"context"
+	"errors"
 	"math"
+	"testing"
 
 	"github.com/gmlewis/modus/runtime/utils"
 	wasm "github.com/tetratelabs/wazero/api"
@@ -27,17 +29,77 @@ type myWasmMockMemory struct {
 	wasm.Memory
 }
 
+func TestMyWasmMock(t *testing.T) {
+	ctx := context.Background()
+	m := &myWasmMock{offset: 46864}
+	block1, _, _ := m.allocateAndPinMemory(ctx, 16, 0)
+	m.Memory().WriteUint32Le(block1, 41)
+	m.Memory().WriteUint32Le(block1+4, 42)
+	m.Memory().WriteUint32Le(block1+8, 43)
+	m.Memory().WriteUint32Le(block1+12, 44)
+	block2, _, _ := m.allocateAndPinMemory(ctx, 160, 242)
+	m.Memory().WriteUint32Le(block2, 161)
+	m.Memory().WriteUint32Le(block2+4, 162)
+	m.Memory().WriteUint32Le(block2+8, 163)
+	m.Memory().WriteUint32Le(block2+12, 164)
+	block3, _, _ := m.allocateAndPinMemory(ctx, 8, 243)
+	m.Memory().WriteUint32Le(block3, 81)
+	m.Memory().WriteUint32Le(block3+4, 82)
+
+	if v, ok := m.Memory().ReadUint32Le(block1); !ok || v != 41 {
+		t.Errorf("expected 41, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block1 + 4); !ok || v != 42 {
+		t.Errorf("expected 42, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block1 + 8); !ok || v != 43 {
+		t.Errorf("expected 43, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block1 + 12); !ok || v != 44 {
+		t.Errorf("expected 44, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block2); !ok || v != 161 {
+		t.Errorf("expected 161, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block2 + 4); !ok || v != 162 {
+		t.Errorf("expected 162, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block2 + 8); !ok || v != 163 {
+		t.Errorf("expected 163, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block2 + 12); !ok || v != 164 {
+		t.Errorf("expected 164, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block3); !ok || v != 81 {
+		t.Errorf("expected 81, got %d", v)
+	}
+	if v, ok := m.Memory().ReadUint32Le(block3 + 4); !ok || v != 82 {
+		t.Errorf("expected 82, got %d", v)
+	}
+}
+
 func (m *myWasmMock) allocateAndPinMemory(ctx context.Context, size, classID uint32) (uint32, utils.Cleaner, error) {
-	m.m = &myWasmMockMemory{
-		offset: m.offset,
-		bytes:  make([]byte, size+8),
+	if size == 0 {
+		return 0, nil, errors.New("size must be greater than 0")
+	}
+	size = 4 * ((size + 3) / 4) // round up to nearest multiple of 4
+
+	offset := m.offset
+	if m.m == nil {
+		m.m = &myWasmMockMemory{
+			offset: m.offset,
+			bytes:  make([]byte, size+8),
+		}
+	} else {
+		offset += uint32(len(m.m.bytes))
+		m.m.bytes = append(m.m.bytes, make([]byte, size+8)...)
 	}
 	refCount := uint32(1)
 	memType := ((size / 4) << 8) | classID
 	m.m.WriteUint32Le(m.offset, refCount)
 	m.m.WriteUint32Le(m.offset+4, memType)
 	// allocateAndPinMemory always returns a pointer _after_ the memory block header
-	return m.offset + 8, nil, nil
+	return offset + 8, nil, nil
 }
 
 func (m *myWasmMock) Memory() wasm.Memory                     { return m.m }
