18b60be7080a5ea7bf8280f2fe88b05832f3fedb
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-03-18 19:32:47 -0700

    Update examples/embedding
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- sdk/moonbit/examples/embedding/build.sh
+++ sdk/moonbit/examples/embedding/build.sh
@@ -5,8 +5,14 @@
 # to invoke the modus-moonbit-build tool with the correct path to your project.
 
 PROJECTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pushd ../../tools/modus-moonbit-build > /dev/null
+pushd ../../../go/tools/modus-moonbit-build > /dev/null
 go run . "$PROJECTDIR"
 exit_code=$?
 popd > /dev/null
+
+if command -v wasm2wat >/dev/null 2>&1; then
+  # If wasm2wat is available, run the command
+  wasm2wat build/embedding.wasm > build/embedding.wat
+fi
+
 exit $exit_code
