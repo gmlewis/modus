e471c4c413c8d7c50c222eeeb31419f0049db794
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-07-11 17:24:35 -0400

    Fix typos that caused unit tests to hang
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- runtime/languages/moonbit/tests/arrays_double_test.go
+++ runtime/languages/moonbit/tests/arrays_double_test.go
@@ -20,7 +20,7 @@ import (
 )
 
 func TestArrayOutput_double_0(t *testing.T) {
-	fnName := "test_fixedarray_output_double_0"
+	fnName := "test_array_output_double_0"
 
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 8=8+words*4), classID=241(Array[Primitive]), words=0, memBlock=[1 0 0 0 241 0 0 0]
 	result, err := fixture.CallFunction(t, fnName)
@@ -41,7 +41,7 @@ func TestArrayOutput_double_0(t *testing.T) {
 }
 
 func TestArrayOutput_double_1(t *testing.T) {
-	fnName := "test_fixedarray_output_double_1"
+	fnName := "test_array_output_double_1"
 
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 16=8+words*4), classID=241(Array[Primitive]), words=1, memBlock=[1 0 0 0 241 1 0 0 0 0 0 0 0 0 240 63]
 	result, err := fixture.CallFunction(t, fnName)
@@ -62,7 +62,7 @@ func TestArrayOutput_double_1(t *testing.T) {
 }
 
 func TestArrayOutput_double_2(t *testing.T) {
-	fnName := "test_fixedarray_output_double_2"
+	fnName := "test_array_output_double_2"
 
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 24=8+words*4), classID=241(Array[Primitive]), words=2, memBlock=[1 0 0 0 241 2 0 0 0 0 0 0 0 0 240 63 0 0 0 0 0 0 0 64]
 	result, err := fixture.CallFunction(t, fnName)
@@ -83,7 +83,7 @@ func TestArrayOutput_double_2(t *testing.T) {
 }
 
 func TestArrayOutput_double_3(t *testing.T) {
-	fnName := "test_fixedarray_output_double_3"
+	fnName := "test_array_output_double_3"
 
 	// memoryBlockAtOffset(offset: 92160=0x00016800=[0 104 1 0], size: 32=8+words*4), classID=241(Array[Primitive]), words=3, memBlock=[1 0 0 0 241 3 0 0 0 0 0 0 0 0 240 63 0 0 0 0 0 0 0 64 0 0 0 0 0 0 8 64]
 	result, err := fixture.CallFunction(t, fnName)
@@ -104,7 +104,7 @@ func TestArrayOutput_double_3(t *testing.T) {
 }
 
 func TestArrayOutput_double_4(t *testing.T) {
-	fnName := "test_fixedarray_output_double_4"
+	fnName := "test_array_output_double_4"
 
 	// memoryBlockAtOffset(offset: 92160=0x00016800=[0 104 1 0], size: 40=8+words*4), classID=241(Array[Primitive]), words=4, memBlock=[1 0 0 0 241 4 0 0 0 0 0 0 0 0 240 63 0 0 0 0 0 0 0 64 0 0 0 0 0 0 8 64 0 0 0 0 0 0 16 64]
 	result, err := fixture.CallFunction(t, fnName)
@@ -125,7 +125,7 @@ func TestArrayOutput_double_4(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_0(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_0"
+	fnName := "test_array_output_double_option_0"
 
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 8=8+words*4), classID=242(Array[String]), words=0, memBlock=[1 0 0 0 242 0 0 0]
 	result, err := fixture.CallFunction(t, fnName)
@@ -146,7 +146,7 @@ func TestArrayOutput_double_option_0(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_1_none(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_1_none"
+	fnName := "test_array_output_double_option_1_none"
 
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 12=8+words*4), classID=242(Array[String]), words=1, memBlock=[1 0 0 0 242 1 0 0 152 41 0 0]
 	// memoryBlockAtOffset(offset: 10648=0x00002998=[152 41 0 0], size: 8=8+words*4), classID=0(Tuple), words=0, memBlock=[255 255 255 255 0 0 0 0]
@@ -168,7 +168,7 @@ func TestArrayOutput_double_option_1_none(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_1_some(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_1_some"
+	fnName := "test_array_output_double_option_1_some"
 
 	// memoryBlockAtOffset(offset: 92160=0x00016800=[0 104 1 0], size: 12=8+words*4), classID=242(Array[String]), words=1, memBlock=[1 0 0 0 242 1 0 0 32 103 1 0]
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 16=8+words*4), classID=1(), words=2, memBlock=[1 0 0 0 1 2 0 0 0 0 0 0 0 0 240 63]
@@ -190,7 +190,7 @@ func TestArrayOutput_double_option_1_some(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_2(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_2"
+	fnName := "test_array_output_double_option_2"
 
 	// memoryBlockAtOffset(offset: 92192=0x00016820=[32 104 1 0], size: 16=8+words*4), classID=242(Array[String]), words=2, memBlock=[1 0 0 0 242 2 0 0 32 103 1 0 0 104 1 0]
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 16=8+words*4), classID=1(), words=2, memBlock=[1 0 0 0 1 2 0 0 0 0 0 0 0 0 240 63]
@@ -213,7 +213,7 @@ func TestArrayOutput_double_option_2(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_3(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_3"
+	fnName := "test_array_output_double_option_3"
 
 	// memoryBlockAtOffset(offset: 92192=0x00016820=[32 104 1 0], size: 20=8+words*4), classID=242(Array[String]), words=3, memBlock=[1 0 0 0 242 3 0 0 32 103 1 0 152 41 0 0 0 104 1 0]
 	// memoryBlockAtOffset(offset: 91936=0x00016720=[32 103 1 0], size: 16=8+words*4), classID=1(), words=2, memBlock=[1 0 0 0 1 2 0 0 0 0 0 0 0 0 38 64]
@@ -240,7 +240,7 @@ func TestArrayOutput_double_option_3(t *testing.T) {
 }
 
 func TestArrayOutput_double_option_4(t *testing.T) {
-	fnName := "test_fixedarray_output_double_option_4"
+	fnName := "test_array_output_double_option_4"
 
 	// memoryBlockAtOffset(offset: 92224=0x00016840=[64 104 1 0], size: 24=8+words*4), classID=242(Array[String]), words=4, memBlock=[1 0 0 0 242 4 0 0 152 41 0 0 32 103 1 0 0 104 1 0 32 104 1 0]
 	// memoryBlockAtOffset(offset: 10648=0x00002998=[152 41 0 0], size: 8=8+words*4), classID=0(Tuple), words=0, memBlock=[255 255 255 255 0 0 0 0]
