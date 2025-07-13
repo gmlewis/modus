e3e6b1b61cb54825eed915eb7b2eebd2afab9537
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-07-13 16:49:16 -0400

    Clean up mbt
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- runtime/languages/moonbit/tests/arrays_bool_test.go
+++ runtime/languages/moonbit/tests/arrays_bool_test.go
@@ -29,8 +29,6 @@ func TestArrayOutput_bool_option_none(t *testing.T) {
 	if result != nil {
 		t.Error("expected a nil result")
 	}
-
-	testInputSide(t, fnName, expected)
 }
 
 func TestArrayOutput_bool_option_some_0(t *testing.T) {
@@ -50,8 +48,6 @@ func TestArrayOutput_bool_option_some_0(t *testing.T) {
 	} else if !reflect.DeepEqual(expected, r) {
 		t.Errorf("expected %v, got %v", expected, r)
 	}
-
-	testInputSide(t, fnName, expected)
 }
 
 func TestArrayOutput_bool_0(t *testing.T) {
