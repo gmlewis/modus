1fe41ac71cf704d66a20abf37b37ccdc39b54e85
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-07-14 10:17:24 -0400

    Add sketch scripts
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ runtime/languages/moonbit/tests/start-sketch-local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+# According to: https://sketch.dev/docs/deployment-modes
+# "local" means to use your local Docker Desktop but use the hosted LLM.
+sketch
---
+++ runtime/languages/moonbit/tests/start-sketch-standalone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+# According to: https://sketch.dev/docs/deployment-modes
+# "standalone" means to use your local Docker Desktop and your own
+# `ANTHROPIC_API_KEY` that must be set before running this script.
+sketch -skaband-addr=""
